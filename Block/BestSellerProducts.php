46b1fc3afdb731eae60eb9bbb86bb5ecde16f4ec
Curtis <32826247+Tuvpt@users.noreply.github.com>
2018-03-15 12:05:06 +0700

    Update BestSellerProducts
    Fix Bestsellers SQL error and Product Url

--- Block/BestSellerProducts.php
+++ Block/BestSellerProducts.php
@@ -1,28 +1,90 @@
 <?php
 namespace Mageplaza\Productslider\Block;
 /**
- * Class FeaturedProducts
+ * Class BestSellerProducts
  * @package Mageplaza\Productslider\Block
  */
 class BestSellerProducts extends \Mageplaza\Productslider\Block\AbstractSlider
 {
-	/**
-	 * get collection of feature products
+    /**
+     * @var \Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable
+     */
+    protected $_catalogProductTypeConfigurable;
+
+    /**
+     * BestSellerProducts constructor.
+     * @param \Magento\Catalog\Block\Product\Context $context
+     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+     * @param \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility
+     * @param \Magento\Framework\App\Http\Context $httpContext
+     * @param \Magento\Rule\Model\Condition\Sql\Builder $sqlBuilder
+     * @param \Magento\CatalogWidget\Model\Rule $rule
+     * @param \Magento\Widget\Helper\Conditions $conditionsHelper
+     * @param \Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable $catalogProductTypeConfigurable
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Catalog\Block\Product\Context $context,
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+        \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility,
+        \Magento\Framework\App\Http\Context $httpContext,
+        \Magento\Rule\Model\Condition\Sql\Builder $sqlBuilder,
+        \Magento\CatalogWidget\Model\Rule $rule,
+        \Magento\Widget\Helper\Conditions $conditionsHelper,
+        \Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable $catalogProductTypeConfigurable,
+        array $data = []
+    )
+    {
+        $this->_catalogProductTypeConfigurable = $catalogProductTypeConfigurable;
+        parent::__construct($context, $productCollectionFactory, $catalogProductVisibility, $httpContext, $sqlBuilder, $rule, $conditionsHelper, $data);
+    }
+
+    /**
+	 * get collection of best-seller products
 	 * @return mixed
 	 */
 	public function getProductCollection()
 	{
 		$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-		$re = $objectManager->create('\Magento\Reports\Model\ResourceModel\Report\Collection\Factory');
-		$resourceCollection = $re->create('Magento\Sales\Model\ResourceModel\Report\Bestsellers\Collection');
-		$resourceCollection->setPageSize($this->getProductsCount());
-		return $resourceCollection;
+		$reportCollection = $objectManager->create('\Magento\Reports\Model\ResourceModel\Report\Collection\Factory');
+		$productCollection = $reportCollection->create('Magento\Sales\Model\ResourceModel\Report\Bestsellers\Collection');
+        $productCollection->setPeriod('month');
+        foreach ($productCollection as $product) {
+            $productIds[]=$this->getProductData($product->getProductId());
+        }
+        $collection      = $objectManager->create('\Magento\Catalog\Model\ResourceModel\Product\Collection')->addIdFilter($productIds);
+        $collection->addMinimalPrice()
+            ->addFinalPrice()
+            ->addTaxPercents()
+            ->addAttributeToSelect('*')
+            ->addStoreFilter($this->getStoreId());
+
+        if ($this->getProductsCount() > $collection->getSize()) {
+            return $collection;
+        } else {
+            return $collection->setPageSize($this->getProductsCount());
+        }
 	}
 
+    /**
+     * @return string
+     */
 	public function getProductCacheKey()
 	{
 		return 'mageplaza_product_slider_bestseller';
 	}
 
-
+    /**
+     * Get Parent Product ID
+     * @param $productID
+     * @return mixed
+     */
+    public function getProductData($id){
+        $parentByChild = $this->_catalogProductTypeConfigurable->getParentIdsByChild($id);
+        if(isset($parentByChild[0])){
+            //set id as parent product id...
+            $id = $parentByChild[0];
+        }
+        return $id;
+    }
 }
