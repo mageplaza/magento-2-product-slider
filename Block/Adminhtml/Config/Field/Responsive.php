efe95ed13e76ee1eef9a0512e1d55ff837e73bdf
Vu Van Tu <tuvv@mageplaza.com>
2020-09-24 14:09:08 +0700

    add validate in responsive field

--- Block/Adminhtml/Config/Field/Responsive.php
+++ Block/Adminhtml/Config/Field/Responsive.php
@@ -34,8 +34,8 @@ class Responsive extends AbstractFieldArray
      */
     protected function _prepareToRender()
     {
-        $this->addColumn('size', ['label' => __('Screen size max'), 'renderer' => false]);
-        $this->addColumn('items', ['label' => __('Number of items'), 'renderer' => false]);
+        $this->addColumn('size', ['label' => __('Screen size max'), 'renderer' => false, 'class' => 'validate-digits']);
+        $this->addColumn('items', ['label' => __('Number of items'), 'renderer' => false, 'class' => 'validate-digits']);
 
         $this->_addAfter       = false;
         $this->_addButtonLabel = __('Add');
